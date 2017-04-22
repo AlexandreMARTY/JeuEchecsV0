1f9f6bbb25c5ec824da0d452e6b27836fe50a9e2
Alexandre <Alexandre@Alexandre-PC>
2017-04-22 20:06:03 +0900

    Création et implémentation de l'interface joueur et de la classe humain

--- src/modelisation/joueurs/Humain.java
+++ src/modelisation/joueurs/Humain.java
@@ -1,5 +1,46 @@
 package modelisation.joueurs;
 
-public class Humain {
+import java.util.List;
 
+import modelisation.pieces.Piece;
+import modelisation.plateau.Case;
+import modelisation.plateau.Couleur;
+import modelisation.plateau.Echiquier;
+
+public class Humain implements Joueur {
+	
+	
+/////////////////////////////////////////Attributs////////////////////////////////////////////
+	private Couleur couleurJoueur; //La couleur du joueur (définit la couleur de ces pièces)
+	private List<Piece> armee; //Son armée de pièces
+	
+	
+/////////////////////////////////////////Getters et Setters////////////////////////////////////////////
+	
+	public Couleur getCouleurJoueur() {
+		return couleurJoueur;
+	}
+	
+	public void setCouleurJoueur(Couleur couleurJoueur) {
+		this.couleurJoueur = couleurJoueur;
+	}
+	
+	public List<Piece> getArmee() {
+		return armee;
+	}
+	
+	public void setArmee(List<Piece> armee) {
+		this.armee = armee;
+	}
+	
+/////////////////////////////////////////Fonctions Interface////////////////////////////////////////////
+	
+	public boolean deplace(Piece piece, Case arrivee, Echiquier plateauJeu) {
+		if (piece.CaseInRayonAction(arrivee)) {
+			piece.setEmplacement(arrivee);
+			arrivee.setOccupeePar(piece);
+			return true;
+		}
+		return false;
+	}	
 }
--- src/modelisation/joueurs/Joueur.java
+++ src/modelisation/joueurs/Joueur.java
@@ -1,5 +1,11 @@
 package modelisation.joueurs;
 
+import modelisation.pieces.Piece;
+import modelisation.plateau.Case;
+import modelisation.plateau.Echiquier;
+
 public interface Joueur {
+	
+	public boolean deplace(Piece piece, Case arrivee, Echiquier plateauJeu);
 
 }
