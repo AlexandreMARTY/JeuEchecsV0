4f56ee0d31ec5a2bd30527385c84dc443f03dbb0
Alexandre <Alexandre@Alexandre-PC>
2017-04-23 11:21:15 +0900

    Début de l'interface

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,24 +2,52 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import modelisation.joueurs.Humain;
+import modelisation.joueurs.Joueur;
+import modelisation.plateau.Couleur;
+import modelisation.plateau.Echiquier;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
+/////////////////////////////////////////Attributs////////////////////////////////////////////
+	private Joueur joueurblanc;
+	private Joueur joueurnoir;
+	private Echiquier plateuJeu;
+	
+	
+/////////////////////////////////////////Constructeur////////////////////////////////////////////
+	public Main() {
+		this.plateuJeu = new Echiquier();
+		this.joueurblanc = new Humain(Couleur.BLANC, this.plateuJeu);
+		this.joueurnoir = new Humain(Couleur.NOIR, this.plateuJeu);
 	}
 	
+/////////////////////////////////////////Getters////////////////////////////////////////////
+	
+	public Joueur getJoueurblanc() {
+		return joueurblanc;
+	}
+
+	public Joueur getJoueurnoir() {
+		return joueurnoir;
+	}
+
+	public Echiquier getPlateuJeu() {
+		return plateuJeu;
+	}
+	
+/////////////////////////////////////////Mise en relation avec l'interface////////////////////////////////////////////
+	
+	
+	
+/////////////////////////////////////////fonction de lancement////////////////////////////////////////////
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
